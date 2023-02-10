2ecb57b177c09cb845304e5c6402066bb17ed334
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-02-10 20:34:03 -0300

    create mapper functions

--- src/main/java/com/rrsys/ordersapi/controllers/OrderController.java
+++ src/main/java/com/rrsys/ordersapi/controllers/OrderController.java
@@ -2,16 +2,11 @@ package com.rrsys.ordersapi.controllers;
 
 import com.rrsys.ordersapi.dtos.OrderDTO;
 import com.rrsys.ordersapi.models.OrderEntity;
-import com.rrsys.ordersapi.models.OrderItemsEntity;
 import com.rrsys.ordersapi.services.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/orders")
@@ -27,25 +22,12 @@ public class OrderController {
 
     @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping
-    public OrderDTO createOrder(@RequestBody OrderDTO orderDto) {
+    public OrderDTO create(@RequestBody OrderDTO orderDto) {
         log.info("creating a order: {}", orderDto.toString());
-        OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setCustomerCPF(orderDto.getCustomerCPF());
-        orderEntity.setTotalAmout(orderDto.getTotalAmout());
-
-        List<OrderItemsEntity> list = orderDto.getItems().stream().map(p-> {
-                    OrderItemsEntity item = new OrderItemsEntity();
-                    item.setProductId(p.getProductId());
-                    item.setQuantity(p.getQuantity());
-                    item.setAmount(p.getAmount());
-                    return item;
-                }).collect(Collectors.toList());
-
-        list.forEach(orderEntity::addOrderItem);
-
+        OrderEntity orderEntity = orderDto.mapperToEntity();
         orderEntity = orderService.create(orderEntity);
 
-        BeanUtils.copyProperties(orderEntity, orderDto);
+        orderDto = OrderDTO.mapperToDto(orderEntity);
 
         log.info("created order success: {}", orderDto);
 
