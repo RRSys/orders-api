ee5a7240a449331fa1ee255a24866c063d3f8816
Rodrigo Lima <rodrigolimasd@gmail.com>
2023-02-09 23:47:44 -0300

    logs to create order

--- src/main/java/com/rrsys/ordersapi/dtos/OrderDTO.java
+++ src/main/java/com/rrsys/ordersapi/dtos/OrderDTO.java
@@ -1,6 +1,9 @@
 package com.rrsys.ordersapi.dtos;
 
+import com.rrsys.ordersapi.enums.OrderStatusEnum;
+
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -9,7 +12,9 @@ public class OrderDTO {
 
     private UUID id;
     private BigDecimal totalAmout;
+    private LocalDateTime date;
     private String customerCPF;
+    private OrderStatusEnum status;
 
     private List<OrderItemsDTO> items = new ArrayList<>();
 
@@ -19,6 +24,14 @@ public class OrderDTO {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
     }
 
     public BigDecimal getTotalAmout() {
@@ -43,6 +56,24 @@ public class OrderDTO {
 
     public void setItems(List<OrderItemsDTO> items) {
         this.items = items;
+    }
+
+    public OrderStatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatusEnum status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderDTO{" +
+                "id=" + id +
+                ", totalAmout=" + totalAmout +
+                ", customerCPF='" + customerCPF + '\'' +
+                ", items=" + items +
+                '}';
     }
 
 }
